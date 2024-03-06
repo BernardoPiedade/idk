68c1ad2020ea5a6d99d8abfcf155fab7e3ae28ef
BernardoPiedade <bernardopiedade@protonmail.com>
2024-03-06 00:36:51 +0000

    Title: Register and Login backend done
    Summary: Registration and login backend are now done.
    		Added email verification, fixed registration to send
    		password to a different table in the database.
    		Added startup script so I don't have t write that php line.
    		Added naming rules so there's no confusion when creating
    		variables names or functions, etc..

--- server-side-php/login.php
+++ server-side-php/login.php
@@ -1,5 +1,5 @@
 <?php include('./conn.php'); // database conection var $conn and session_start() ?>
-<?php include('./errors-login.php'); // errors handling array ?>
+<?php include('./errorsHandling/errors-login.php'); // errors handling array ?>
 <?php
 
 
@@ -17,10 +17,12 @@ if(isset($_POST['login_user'])){
 	// verify both fields were filled
 	if (empty($username_entered)) {
 		array_push($errors, "Username is required!");
+		exit();
 	}
 	
 	if (empty($password_entered)) {
 		array_push($errors, "Password is required!");
+		exit();
 	}
 
 	// if there's no errors above, check the username and the password
@@ -35,24 +37,18 @@ if(isset($_POST['login_user'])){
 		$find_pass_hash = $conn->prepare("SELECT uPass FROM pass WHERE userId=:username_id");
 		$find_pass_hash->execute(['username_id' => $get_find_user]);
 
-		// verify if the entered password matches with the hash
-		
-		//if ()
+		// verify if the entered password matches with the stored hash
+		// if it matches, the user is logged in and sent to homepage
+		if (password_verify($password_entered, $find_pass_hash)){
+			$_SESSION['username'] = $username_entered;
+			$_SESSION['success'] = "You are now logged in!";
 
+			exit();
 
-
-		$query = "SELECT * FROM users WHERE username='$username'";
-		$run_query = mysqli_query($db, $query);
-		$result = mysqli_fetch_assoc($run_query);
-		$get_pwd = $result['pass'];
-
-		if (password_verify($password, $get_pwd)) {
-			$_SESSION['username'] = $username;
-			$_SESSION['success'] = "You are now logged in";
-
-			header('location: index.php');
-		} else {
-			array_push($errors, "Invalid Username or Password");
+			header("location: homepage.php");
+		}else{
+			array_push($errors, "Invalid Username or Password!");
+			exit();
 		}
 
 	}
--- server-side-php/register-acc.php
+++ server-side-php/register-acc.php
@@ -1,6 +1,6 @@
 <?php include('./conn.php'); // database conection var $conn and session_start()  ?>
 <?php include('../conf.php'); // password $pepper ?>
-<?php include('errors-register-acc.php'); // errors handling array ?>
+<?php include('./errorsHandling/errors-register-acc.php'); // errors handling array ?>
 <?php
 
 // form entries initialization
@@ -21,26 +21,38 @@ if (isset($_POST['reg_user'])){
 	// verify the entries were filled
 	if (empty($new_username)){
 		array_push($errors, "Username is required!");
+		exit();
 	}
 
 	if (empty($new_email)){
 		array_push($errors, "Email is required!");
+		exit();
+	}
+
+	// verify if email is valid
+	if(filter_var($new_email, FILTER_VALIDATE_EMAIL) == false){
+		array_push($errors, "Email address is not valid!");
+		exit();
 	}
 
 	if (empty($new_password1)){
 		array_push($errors, "Password is required!");
+		exit();
 	}
 
 	if(strlen($new_password1) < 10){
 		array_push($errors, "Password must be at least 10 characters long!");
+		exit();
 	}
 
 	if (empty($new_password2)){
 		array_push($errors, "Password verification is required!");
+		exit();
 	}
 
 	if ($new_password1 != $new_password2){
 		array_push($errors, "The two passwords do not match!");
+		exit();
 	}
 
 	// check if username or email is already in use
@@ -62,17 +74,30 @@ if (isset($_POST['reg_user'])){
 	// if there's no erros, register the user
 	if(count($errors) == 0){
 
+		// to register the user we first add it to the "users" table
+		// we then need to get the "userId" that was given
+		// having that, we can then add the password to the "pass" table
+
 		// encrypt password
 		$password_peppered = hash_hmac("sha256", $new_password1, $pepper);
 		$password_hashed = password_hash($pwd_peppered, PASSWORD_ARGON2ID);
 
-		// prepare and execute query
-		$register_user = $conn->prepare("INSERT INTO users (username, pass, email, creationDate, uRank) VALUES(:new_username, :password_hashed, :new_email, NOW(), 0");
-		$register_user->execute(['new_username' => $new_username, 'password_hashed' => $password_hashed, 'new_email' => $new_email]);
+		// prepare and execute query to add user to database
+		$register_user = $conn->prepare("INSERT INTO users (username, email, creationDate, uRank) VALUES (:new_username, :new_email, NOW(), 0");
+		$register_user->execute(['new_username' => $new_username, 'new_email' => $new_email]);
+
+		// prepare and execute query to get the user id
+		$find_user_id = $conn->prepare("SELECT id FROM users WHERE username=:new_username");
+		$find_user_id->execute(['username_entered' => $new_username]);
+		$get_find_user_id = $find_user_id->fetchAll();
+
+		// prepare and execute query to add user password to database
+		$register_password = $conn->prepare("INSERT INTO pass (uPass, userId) VALUES (:password_hashed, :user_id)");
+		$register_password->execute(['password_hashed' => $password_hashed, 'user_id' => $get_find_user_id]);
 		
 		// login and send user to homepage
 		$_SESSION['username'] = $new_username;
-		$_SESSION['success'] = "You're logged in!";
+		$_SESSION['success'] = "You're now logged in!";
 		header('location: index.php');
 
 	}
